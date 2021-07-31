1f4ae4b58c91f167b9a24228c9f3ecc2ed889056
rohira-dhruv <f20201725@goa.bits-pilani.ac.in>
2021-07-31 21:12:55 +0530

    Created the game of Higher or Lower

---
+++ HigherLower.py
@@ -0,0 +1,47 @@
+import random
+from art import logo_higher_lower,vs
+from data import data_higher_lower
+# from replit import clear
+
+def get_item(data_list, removal_list):
+    item = data_list[random.randint(0,len(data_list))]
+    removal_list.append(item)
+    data_list.remove(item)
+    return item
+
+def compare(item_A, item_B):
+    if item_A['follower_count'] >= item_B['follower_count']:
+        return 'A'
+    else:
+        return 'B'
+    
+def game():
+    print(logo_higher_lower)
+    removed_items = []
+    end_game = False
+    score = 0
+    item_A = get_item(data_higher_lower, removed_items)
+    while not end_game:
+        # print(logo_higher_lower)
+        item_B = get_item(data_higher_lower, removed_items)
+        print(f"Compare A: {item_A['name']}, a {item_A['description']}, from {item_A['country']}")
+        print(vs)
+        print(f"Against B: {item_B['name']}, a {item_B['description']}, from {item_B['country']}")
+        higher = input("Who has more followers? 'A' or 'B': ").upper()
+        # clear()
+        if higher == compare(item_A, item_B):
+            score += 1
+            print(f"\nYou're right! The current score is: {score}")
+        else: 
+            end_game = True
+            print(f"\nSorry that's wrong. Your final score is: {score}")
+        item_A = item_B
+    data_higher_lower.extend(removed_items)
+
+game()
+
+
+
+
+
+
